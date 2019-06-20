d4e31c48b843fa2f4978552bcce8a5023ba9cbd5
Manik Malik <32712311+manikmalik1999@users.noreply.github.com>
2019-06-20 15:42:40 +0530

    Update temp.js

--- API/Routes/temp.js
+++ API/Routes/temp.js
@@ -1,36 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require("mongoose");
 
+const Temper = require("../models/temper");
 // Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
-    });
-});
+    Temper.find()
+    .select('value startTime endTime')
+      .exec()
+      .then(docs => {
+        const response = {
+          products: docs
+        };
+       
+        res.status(200).json(response);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+
+
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order was created',
-        order: order
+    const Temperature = new Temper({
+        _id: new mongoose.Types.ObjectId(),
+        startTime: req.body.startTime,
+        endTime: req.body.endTime,
+        value: req.body.value
+      });
+      Temperature
+        .save()
+        .then(result => {
+          console.log(result);
+          res.status(201).json({
+            message: "DATA ADDED",
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({
+            error: err
+          });
+        });
     });
-});
+// router.get('/:orderId', (req, res, next) => {
+//     res.status(200).json({
+//         message: 'Order details',
+//         orderId: req.params.orderId
+//     });
+// });
 
-router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderId: req.params.orderId
-    });
-});
-
-router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    });
-});
+// router.delete('/:orderId', (req, res, next) => {
+//     res.status(200).json({
+//         message: 'Order deleted',
+//         orderId: req.params.orderId
+//     });
+// });
 
 module.exports = router;
